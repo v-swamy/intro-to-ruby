674963077181deff3e7939355605ebba51c3d28e
Vikram Swamy <vswamy@gmail.com>
2014-12-16 13:51:18 -0500

    Add Intro to Ruby files

---
+++ method.rb
@@ -0,0 +1,8 @@
+def some_method(number)
+	number = 7
+end
+
+a = 5
+some_method(a)
+puts a
+
---
+++ name.rb
@@ -0,0 +1,11 @@
+puts "What is your first name?"
+first_name = gets.chomp
+puts "What is your last name?"
+last_name = gets.chomp
+puts first_name + ' ' + last_name
+
+# puts "Nice to meet you, " + name
+# 10.times do
+# 	puts name
+# end
+
