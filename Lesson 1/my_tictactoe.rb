a3c1c6a40dbc39044ec61604d036cf0f20b6280a
Vikram Swamy <vswamy@gmail.com>
2014-12-29 22:53:15 -0500

    Updating tictactoe file

--- Lesson 1/my_tictactoe.rb
+++ Lesson 1/my_tictactoe.rb
@@ -1,80 +1,65 @@
-# Display empty board
 
-# Loop until winner or full board
-# # Ask for user input for empty square (X)
-# check for winner or full board
-# # Computer will pick an empty square (O)
-# check for winner or full board
-
-# 4.)  Alternate between computer and user until board is full
-# 5.)  Or until 3 in a row
-
-# if there is a winner, show winner, 
-# else it is a tie
-
-#initialize the board
 require 'pry'
 
-
-
 #draw the board
-def draw_board(b)
+def draw_board(board)
   system 'clear'
-  puts " #{b[1]} | #{b[2]} | #{b[3]} "
+  puts " #{board[1]} | #{board[2]} | #{board[3]} "
   puts "---+---+---"
-  puts " #{b[4]} | #{b[5]} | #{b[6]} "
+  puts " #{board[4]} | #{board[5]} | #{board[6]} "
   puts "---+---+---"
-  puts " #{b[7]} | #{b[8]} | #{b[9]} "
+  puts " #{board[7]} | #{board[8]} | #{board[9]} "
 end
 
 #find empty spaces
-def empty_spaces(b)
-  b.select { |k,v| v == ' '}
+def empty_spaces(board)
+  board.select { |key,value| value == ' '}
 end
 
 #user selects a space
-def user_select_space(b)
+def user_select_space(board)
   puts "Select a space (1 - 9)"
   user_selection = gets.chomp.to_i
-  while !empty_spaces(b).keys.include?(user_selection)
+  while !empty_spaces(board).keys.include?(user_selection)
     puts "That space is taken! Please select another space (1 - 9)"
     user_selection = gets.chomp.to_i
   end
-  b[user_selection] = 'X'
+  board[user_selection] = 'X'
 end
 
 WINNING_ROWS = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
 
 #check if there is a winner
-def find_winner(b)
+def find_winner(board)
   WINNING_ROWS.each do |line|
-    return "Player" if b.values_at(*line).count('X') == 3
-    return "Computer" if b.values_at(*line).count('O') == 3
+    return "Player" if board.values_at(*line).count('X') == 3
+    return "Computer" if board.values_at(*line).count('O') == 3
   end
   nil
 end
 
 #find the best available space for computer.
-def find_best_space(b)
+def find_space_to_win(board, mark)
   WINNING_ROWS.each do |line|
-    if line.select { |x| b[x] == 'O' }.length == 2 && line.select { |x| b[x] == ' ' }.length == 1
-      choice_key = line.find { |i| b[i] == ' ' }
+    if line.select { |row| board[row] == mark }.length == 2 && line.select { |row| board[row] == ' ' }.length == 1
+      choice_key = line.find { |space| board[space] == ' ' }
       return choice_key
     end
   end
-  WINNING_ROWS.each do |line|
-    if line.select { |x| b[x] == 'X' }.length == 2 && line.select { |x| b[x] == ' ' }.length == 1
-      choice_key = line.find { |i| b[i] == ' ' }
-      return choice_key
-    end
-  end
-  choice_key = empty_spaces(b).keys.sample
-  return choice_key
+  nil
 end
 
+
 #computer selects a space
-def computer_select_space(b)
-  b[find_best_space(b)] = 'O'
+def computer_select_space(board)
+  if find_space_to_win(board, 'O')
+    board[find_space_to_win(board, 'O')] = 'O'
+  elsif find_space_to_win(board, 'X')
+    board[find_space_to_win(board, 'X')] = 'O'
+  else
+    choice_key = empty_spaces(board).keys.sample
+    board[choice_key] = 'O'
+  end
 end
 
 #array of players
@@ -85,9 +70,9 @@ continue_playing = 'y'
 while continue_playing == 'y'
 
   #create a new board
-  board = {}
-  (1..9).each { |n| board[n] = " " }
-  draw_board(board)
+  game_board = {}
+  (1..9).each { |space| game_board[space] = " " }
+  draw_board(game_board)
 
   #set the starting player
   starting_player = PLAYERS.sample
@@ -95,26 +80,26 @@ while continue_playing == 'y'
   sleep 2
 
   if starting_player == "Player"
-    until empty_spaces(board).empty? || find_winner(board)
-      user_select_space(board)
-      draw_board(board)
-      computer_select_space(board)
-      draw_board(board)
+    until empty_spaces(game_board).empty? || find_winner(game_board)
+      user_select_space(game_board)
+      draw_board(game_board)
+      computer_select_space(game_board)
+      draw_board(game_board)
     end
   else
-    computer_select_space(board)
-    draw_board(board)
-    until empty_spaces(board).empty? || find_winner(board)
-      user_select_space(board)
-      draw_board(board)
-      computer_select_space(board)
-      draw_board(board)
+    computer_select_space(game_board)
+    draw_board(game_board)
+    until empty_spaces(game_board).empty? || find_winner(game_board)
+      user_select_space(game_board)
+      draw_board(game_board)
+      computer_select_space(game_board)
+      draw_board(game_board)
     end
   end
 
-  if find_winner(board)
-    puts "Winner: #{find_winner(board)}"
-  elsif empty_spaces(board).empty?
+  if find_winner(game_board)
+    puts "Winner: #{find_winner(game_board)}"
+  elsif empty_spaces(game_board).empty?
     puts "It's a tie!"
   end
 
